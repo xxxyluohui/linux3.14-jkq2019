46cc2ead40e3af35c2be06667ed96788ce2685cf
xxxyluohui <xxxyluohui@163.com>
2020-07-16 11:29:45 +0800

    add linux code

---
+++ env.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/usr/local/arm/gcc-linaro-arm-linux-gnueabihf-4.9-2014.07_linux/bin
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export ARCH=arm
---
+++ mjy_set_env.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+export CROSS_COMPILE=arm-poky-linux-gnueabi-
+export ARCH=arm
+export PATH=$PATH:/usr/local/games:/usr/local/arm/poky/1.8/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/
